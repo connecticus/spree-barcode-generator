33b54342b9a2190acd2a5217bc0036a03ac1d706
Enrique Alvarez <enriqueaf@gmail.com>
2013-10-07 02:47:27 +0200

    Corrected routes, problems with rails namespace

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,14 @@ Spree::Core::Engine.routes.append do
     scope :pos do
       get "/new" => "pos#new"
       get "/show/:number" => "pos#show"
+      post "/show/:number" => "pos#show"
       get "/find/:number" => "pos#find"
       get "/add/:number/:item" => "pos#add"
       get "/remove/:number/:item" => "pos#remove"
       get "/print/:number" => "pos#print"
       get "/inventory/:number" => "pos#inventory"
-      get "/" , :to => "pos#new"
     end
+    get "/pos" , :to => "pos#new"
   end
 end
 
