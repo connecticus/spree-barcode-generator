e2843fbb45d0eaec975020f39513665b4ec9895f
Enrique Alvarez <enrique@codecantor.com>
2014-01-08 14:49:42 +0100

    Corrected link!

--- app/overrides/show_stock_admin_tab.rb
+++ app/overrides/show_stock_admin_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "show_stock_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => " <%= tab( :stock , :url => spree.admin_pos_show_stock_path, icon: 'icon-shopping-cart') %>",
+                     :text => " <%= tab( :stock , :url => spree.admin_stock_products_path, icon: 'icon-shopping-cart') %>",
                      :disabled => false)
