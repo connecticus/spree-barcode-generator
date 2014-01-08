d338af29fb0b64a30f68961a6abae5e7162947c7
Enrique Alvarez <enrique@codecantor.com>
2014-01-08 18:41:59 +0100

    Create coupon in refund. Note that usage_limit must be 2 althougt you use it 1 time!

--- app/controllers/spree/admin/refund_controller.rb
+++ app/controllers/spree/admin/refund_controller.rb
@@ -23,13 +23,27 @@ class Spree::Admin::RefundController < Spree::Admin::BaseController
   end
 
   def create_refund
+    @order = Spree::Order.find_by(number: params[:order_number])
     @return_authorization = Spree::ReturnAuthorization.find_by(number: params[:rma])
+    @coupon_amount = params[:coupon_amount].try(&:to_f)
+
+    if @coupon_amount && @coupon_amount > 0
+      create_discount_coupon  @coupon_amount
+      @return_authorization.reason = "POS refund. Create coupon #{@coupon.code} with the amount #{params[:coupon_amount]}"
+    end
+
     (params[:return_quantity] || []).each { |variant_id, qty| @return_authorization.add_variant(variant_id.to_i, qty.to_i) }
     @return_authorization.receive
   end
 
   protected
-  def create_discount_coupon
-    @coupon = Spree::Promotion
+  def create_discount_coupon(amount)
+    @coupon = Spree::Promotion.create(name: "REFUND #{@order.number}",event_name: 'spree.checkout.coupon_code_added', usage_limit: 2)
+    @coupon.code = "#{@return_authorization.number}NI"
+    action = @coupon.actions.build(type: 'Spree::Promotion::Actions::CreateAdjustment')
+    calculator = Spree::Calculator::FlatRate.create
+    action.calculator = calculator
+    @coupon.save!
+    calculator.preferred_amount = amount
   end
 end
