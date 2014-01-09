4c6a8ff4b76cfd7536893ebb35224e7cdef8e4dc
Enrique Alvarez <enrique@codecantor.com>
2014-01-09 23:31:18 +0100

    Modified on stock page!

--- app/controllers/spree/admin/show_stock_controller.rb
+++ app/controllers/spree/admin/show_stock_controller.rb
@@ -1,5 +1,7 @@
 class Spree::Admin::ShowStockController < Spree::Admin::BaseController
   def index
-    @products = Spree::Product.all.includes(variants: :option_values)
+    @products = Spree::Product.all.includes(variants: :option_values).select do |product|
+      product.total_on_hand > 0
+    end
   end
 end
