1aa5f8ee05750156369ac3e4334ab50adeb986ab
k@connecticus.org <Ekaterina Belinska>
2014-09-01 17:40:04 +0400

    label markup

--- app/controllers/spree/admin/barcode_controller.rb
+++ app/controllers/spree/admin/barcode_controller.rb
@@ -3,6 +3,7 @@ require 'barby'
 require 'prawn'
 require 'prawn/measurement_extensions'
 require 'barby/barcode/code_128'
+require 'barby/barcode/ean_13'
 require 'barby/outputter/png_outputter'
 class Spree::Admin::BarcodeController < Spree::Admin::BaseController
   before_filter :load 
@@ -10,34 +11,39 @@ class Spree::Admin::BarcodeController < Spree::Admin::BaseController
   
   # moved to pdf as html has uncontrollable margins
   def print
-    pdf = Prawn::Document.new( :page_size => [ 54.mm , 25.mm ] , :margin => 1.mm )
+    pdf = Prawn::Document.new( :page_size => [ 54.mm , 25.mm ] , :margin => 2.mm )
+    pdf.font 'app/assets/stylesheets/store/OpenSans-Light.ttf'
 
     option_value = " #{@variant.option_values.first.presentation}" if @variant.option_values.first
     name_show = @variant.product.name
     price = @variant.display_price.to_s
 
     pdf.float do
-      pdf.text name_show, align: :left
-      pdf.text @variant.barcode, align: :left
+      pdf.draw_text name_show, :at => [2,52], :size => 9
+      pdf.draw_text @variant.barcode, :at => [2,32], :size => 10
     end
-    pdf.text option_value, align: :right if option_value
-    pdf.text price, align: :right
+    pdf.draw_text option_value, :at => [0,43], :size => 9
+    pdf.draw_text price, :at => [90,32], :size => 10
 
     if barcode = get_barcode
-      pdf.image( StringIO.new( barcode.to_png(:xdim => 5)) , :width => 50.mm , 
+      pdf.image( StringIO.new( barcode.to_png(:xdim => 5)) , :width => 50.mm,
             :height => 10.mm , :at => [ 0 , 10.mm])
     end
-    send_data pdf.render , :type => "application/pdf" , :filename => "#{name_show}.pdf"
+    send_data pdf.render , :type => "application/pdf" , :filename => "#{@variant.barcode}.pdf"
   end
     
   
   private
-
-  #get the barby barcode object from the id, or nil if something goes wrong
-  def get_barcode
-    code  = @variant.barcode
-    return nil if code.to_s.empty?
-    ::Barby::Code128B.new( code  )
+   def get_barcode
+    code = nil
+    code = @variant.ean if @variant.respond_to?(:ean)
+    code = @variant.sku if (code == nil) or (code == "")
+    return nil if (code == nil) or (code == "")
+    if code.length == 12
+      return ::Barby::EAN13.new( code )
+    else
+      return ::Barby::Code128B.new( code  )
+    end
   end
   
   def load
